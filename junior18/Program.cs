899d0e94c18dc873970c0760961dfdd215eeb80e
Danila <asd@mail.ru>
2023-04-16 15:25:58 +0300

    Добавьте файлы проекта.

---
+++ junior18/Program.cs
@@ -0,0 +1,33 @@
+﻿string bracketExpression = "( () ( () ) )"; 
+
+int count = 0;
+int maxCount = 0;
+
+foreach (var bracket in bracketExpression)
+{
+    if (bracket == '(')
+    {
+        count++;
+
+        if (count > maxCount)
+        {
+            maxCount = count;
+            Console.WriteLine(maxCount);
+        }
+    }
+    else if (bracket == ')')
+    {
+        count--;
+
+        if (count < 0)
+        {
+            Console.WriteLine("Некорректное скобочное выражение");
+            break;
+        }
+    }
+}
+
+if (maxCount > 0 && count == 0)
+{
+    Console.WriteLine("Максимальная глубина: " + maxCount);
+}
